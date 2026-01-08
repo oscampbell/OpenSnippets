52e1cd756a8d48b8189b351177bcb77f237a165c
Oliver Campbell <os.campbell@protonmail.com>
2026-01-08 08:42:19 +0000

    Initial Commit

---
+++ OpenSnippets/OpenSnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSnippetsApp.swift
+//  OpenSnippets
+//
+//  Created by Oliver Campbell on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenSnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
